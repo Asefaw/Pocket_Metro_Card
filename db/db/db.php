68696490d2518978cc3c0abe3d256af19451de00
Asefaw <asefaw.mekuria@mail.citytech.cuny.edu>
2016-06-01 22:31:22 -0400

    Update db.php

--- db/db/db.php
+++ db/db/db.php
@@ -1,8 +1,8 @@
 <?php
-$mysql_host = "mysql2.000webhost.com";
-$mysql_database = "a1409470_mta";
-$mysql_user = "a1409470_fatima";
-$mysql_password = "fatima123";
+$mysql_host = " ";
+$mysql_database = "";
+$mysql_user = " ";
+$mysql_password = " ";
 
 
 $db = mysql_connect("$mysql_host", "$mysql_user", "$mysql_password")or die("cannot connect server "); 
