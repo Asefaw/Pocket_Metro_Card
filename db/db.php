ebf558a1c2d1eb81cfb0513302e531fff6a5fab3
Asefaw <asefaw.mekuria@mail.citytech.cuny.edu>
2016-06-01 22:30:42 -0400

    Update db.php

--- db/db.php
+++ db/db.php
@@ -1,15 +1,15 @@
 <?php
-$mysql_host = "mysql.hostinger.co.uk";
-$mysql_database = "u521351239_mta";
-$mysql_user = "u521351239_root";
-$mysql_password = "MetroCard";
+$mysql_host = "";
+$mysql_database = "";
+$mysql_user = "";
+$mysql_password = "";
 
 
 
 
 
 try {
-    $conn = new PDO("mysql:host=$mysql_host;dbname=u521351239_mta", $mysql_user, $mysql_password);
+    $conn = new PDO("mysql:host=$mysql_host;mysql_host", $mysql_user, $mysql_password);
    
 } catch (PDOException $e) {
     print "Error!: " . $e->getMessage() . "<br/>";
